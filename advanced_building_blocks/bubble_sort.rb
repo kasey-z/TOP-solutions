837992e4fd51421ce88f422c379af5eccac34004
Kasey Z <qicong.zheng@gmail.com>
2017-06-12 11:46:09 +0800

    solution for buble sort

---
+++ advanced_building_blocks/bubble_sort.rb
@@ -0,0 +1,46 @@
+def bubble_sort (arr)
+  i = 0
+  (arr.length-1).times do
+      x=0
+      y=1
+      while y < (arr.length-i)
+        if arr[x] > arr[y]
+          bigger=arr[x]
+          arr[x]=arr[y]
+          arr[y]=bigger
+        end
+        x += 1
+        y = x+1
+      end
+      i += 1
+   end
+   puts "the bubble_sort method result is"
+   p arr
+end
+bubble_sort([4,3,78,2,0,2])
+
+#---------------------------------------------------------------------------
+
+def bubble_sort_by (arr)
+  i = 0
+  (arr.length-1).times do
+      x = 0
+      y = 1
+      while y < (arr.length - i)
+        if (yield arr[x], arr[y]) > 0
+          bigger=arr[x]
+          arr[x]=arr[y]
+          arr[y]=bigger
+        end
+        x += 1
+        y = x + 1
+      end
+      i += 1  #after each round, one of number in the right side in fixed. won't count this number in next round
+   end
+   puts "the bubble_sort_by method result is"
+   p arr
+end
+
+bubble_sort_by ([4,3,78,2,0,2]) do |left, right|
+     left - right
+end

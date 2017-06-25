61e31dded3fdcacd599c118cde23dc7672532b82
Kasey Z <qicong.zheng@gmail.com>
2017-06-25 21:15:06 +0800

    add saving function

---
+++ file_io_and_serialization/hangman/README.md
@@ -0,0 +1,4 @@
+#This is a simple command line game called Hangman.
+#If you're unfamiliar with how Hangman works, see Wikipedia.
+#cd to the root dir. Then run the ruby file by "ruby lib/hangman.rb"
+#You may save the game if you want, and you can choose jump exactly back to where you were saved.
--- file_io_and_serialization/hangman/lib/hangman.rb
+++ file_io_and_serialization/hangman/lib/hangman.rb
@@ -1,24 +1,32 @@
 class Hangman
+  require 'yaml'
+
   def initialize
     @chance = 5
     @word = get_random_word
-    @human = Human.new
     @word_result = '_ ' * @word.length
+    @correct = []
+    @miss = []
+  end
+
+  def from_yaml
+    file = YAML.load_file('save_data.yml')
+    @chance = file[:chance]
+    @word = file[:word]
+    @miss = file[:miss]
+    @correct = file[:correct]
+    @word_result = file[:word_result]
   end
 
   def start_gaming
     display_result
     until game_end?
+      ask_save
       human_guess
       compare
       display_result
     end
-
-    if win?
-      puts "Congratulations! You win!"
-    else
-      puts "Out of guesses! You lose!"
-    end
+    puts win? ? "Congratulations! You win!" : "Out of guesses! You lose!"
     puts "The answer is #{@word.join.upcase}."
   end
 
@@ -26,11 +34,30 @@ class Hangman
 
   def get_random_word
     words = File.readlines('5desk.txt')
-    word = String.new
-    until (5..6).include?(word.length)
-      word = words[rand(words.length)].strip
-    end
+    word = ''
+    word = words[rand(words.length)].strip until (5..12).include?(word.length)
     word = word.split('')
+  end
+
+  def ask_save
+     puts "Do you want to quit and save the game?"
+     puts "If yes, input 'Y'. If not, press ENTER:"
+     answer = gets.chomp.downcase
+     if answer == 'y'
+       save_game
+       exit
+     end
+  end
+
+  def save_game
+    yaml_file = YAML.dump({
+      word: @word,
+      chance: @chance,
+      word_result: @word_result,
+      miss: @miss,
+      correct: @correct
+    })
+    File.open('save_data.yml', 'w') { |f| f.write yaml_file }
   end
 
   def human_guess
@@ -45,67 +72,55 @@ class Hangman
 
   def compare
     if @word.join.downcase.include?(@letter)
-      @human.correct.push @letter
-      @word_result = get_word_result(@human.correct)
+      @correct.push @letter
+      @word_result = get_word_result(@correct)
     else
-      @human.miss.push @letter
+      @miss.push @letter
       @chance -= 1
     end
   end
 
   def get_word_result(arr)
-    result_arr = @word.collect do |character|
-      if arr.include?(character.downcase)
-        character
-      else
-        '_'
-      end
-    end
-    result_arr = result_arr.join(' ')
+    result_arr = @word.collect{ |character| arr.include?(character.downcase) ? character : '_' }
+    result_string = result_arr.join(' ')
   end
 
   def display_result
+    system('cls')
+    puts ''
+    puts "Your guesses result:"
+    puts ''
     puts "Word: #{ @word_result }"
-    puts "Misses: #{ @human.miss.join(', ') }"
+    puts "Misses: #{ @miss.join(', ') }"
     puts "Chances left: #{ @chance }"
+    puts ''
   end
 
   def game_end?
-    if @chance == 0 || win?
-      true
-    else
-      false
-    end
+    ( @chance == 0 || win? ) ? true : false
   end
 
   def win?
-    if @word.join(' ') == @word_result
-      true
-    else
-      false
-    end
+    @word.join(' ') == @word_result ? true : false
   end
 
 end
 
-class Human
-  attr_accessor :correct, :miss
 
-  def initialize
-    @correct = []
-    @miss = []
-  end
+play_again = 'y'
+while play_again == 'y'
+  system('cls')
+  puts ''
+  puts "                          ~ Hangman ~"
+  puts "Hangman is a guessing game between you and the computer."
+  puts "The computer will thinks of a word with 5 to 12 characters."
+  puts "In every turn you could make a guess of a letter."
+  puts "You will have 5 chances for incorrect guesses."
+  puts "For new game, press ENTER. Open saved game, input Y :"
+  react = gets.chomp.downcase
+  game = Hangman.new
+  game.from_yaml if react == 'y'
+  game.start_gaming
+  puts "Do you want to play again? If yes, input Y. If no, press ENTER :"
+  play_again = gets.chomp.downcase
 end
-
-
-system('cls')
-puts ''
-puts "                          ~ Hangman ~"
-puts "Hangman is a guessing game between you and the computer."
-puts "The computer will thinks of a word with 5 to 12 characters."
-puts "In every turn you could make a guess of a letter."
-puts "You will have 5 chances for incorrect guesses."
-puts "Press enter if you are ready :"
-react = gets.chomp
-game = Hangman.new
-game.start_gaming
